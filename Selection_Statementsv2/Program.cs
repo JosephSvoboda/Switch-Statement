7d42a67598ea95874d9fadadbffc61b050716eb0
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-07 14:12:34 -0400

    Second exercise commit

---
+++ Selection_Statementsv2/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.WriteLine("What is your favorite school subject?");
+var userInput = Console.ReadLine();
+
+switch (userInput)
+{
+    case "Math":
+        Console.WriteLine("My favorite!");
+        break;
+
+    case "Science":
+        Console.WriteLine("Very nice.");
+        break;
+
+    case "English":
+        Console.WriteLine("Eh, my least favorite");
+        break;
+
+    case "Band":
+        Console.WriteLine("Is this even a real subject?");
+        break;
+
+    case "P.E.":
+        Console.WriteLine("Ah, the best of the best!");
+        break;
+        
+        default:
+        Console.WriteLine("Try again.");
+        break;
+}
